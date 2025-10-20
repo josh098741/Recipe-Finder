44755d12ec9975f3dee53c49789cc1a60767b98d
josh098741 <morongemokaya@gmail.com>
2025-10-20 13:03:51 +0300

    Additional commits to Header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,16 @@
+
+
+
+function Header(){
+
+    return(
+        <div className="h-[100px] bg-gradient-to-r from-blue-500 to-green-500 flex justify-center items-center">
+            <div className="flex flex-col justify-center items-center">
+                <h1 className="text-xl">Recipe Finder</h1>
+                <p className="text-xl">Fast Convinient Meals At Your Fingure tips</p>
+            </div>
+        </div>
+    );
+}
+
+export default Header
